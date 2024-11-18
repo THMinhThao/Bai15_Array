35c520c858265afdbd65ba42efed9a2645b16599
THMinhThao <thaominh245@gmail.com>
2024-11-18 14:36:56 +0700

    Add project files.

---
+++ Bai15_Array.cpp
@@ -0,0 +1,18 @@
+// Bai15_Array.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   // Create an arry of strings
+	string cars[5] = { "Volvo", "BMW", "Ford", "Mazda", "tesla" };
+
+	// Loop through strings 
+	for (int i = 0; i < 5; i++)
+	{
+		cout << cars[i] << "\n";
+	}
+}
+
+
